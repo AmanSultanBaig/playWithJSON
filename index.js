e549d11a730f5ead8bc1283e6621e2b69c7d3f49
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-02-03 22:27:43 +0500

    dummy json configure

--- index.js
+++ index.js
@@ -1,1 +1,130 @@
-object = [];
+object = [
+  {
+    "name": "Aman",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company1",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL",
+      "MONGODB",
+      "NodeJS"
+    ]
+  }],
+  },
+  {
+    "name": "Noman",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company2",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  },
+  {
+    "name": "Salman",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company3",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  },
+  {
+    "name": "Usman",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company3",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  },
+  {
+    "name": "Nabeel",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company3",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  },
+  {
+    "name": "Talha",
+    "address": "ABC",
+    "city": "karachi",
+    "work" : [{
+      "company": "ABC Company3",
+      "position": "Developer",
+      "website": "www.web.com",
+    }],
+    "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  },
+];
